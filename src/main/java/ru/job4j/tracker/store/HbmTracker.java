218ecebd704e2551560f004eef351dcbcac681e2
kirillbelyaev74 <kirbel74@gmail.com>
2021-04-09 13:15:53 +0500

    1. HbmTracker.[287705#449926]

--- src/main/java/ru/job4j/tracker/store/HbmTracker.java
+++ src/main/java/ru/job4j/tracker/store/HbmTracker.java
@@ -12,70 +12,75 @@ import java.util.List;
 
 public class HbmTracker {
 
-    private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+    private static final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
             .configure().build();
-    private final SessionFactory sf = new MetadataSources(registry)
+    private static final SessionFactory sf = new MetadataSources(registry)
             .buildMetadata().buildSessionFactory();
 
     public Items add(Items items) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(items);
-        session.getTransaction().commit();
-        session.close();
+        try (Session session = sf.openSession()) {
+            session.beginTransaction();
+            session.save(items);
+            session.getTransaction().commit();
+        }
         return items;
     }
 
     public boolean replace(String id, Items items) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Query query = session.createQuery("update Items set name =: name, created =: created, description =: description where id =: id");
-        query.setParameter("name", items.getName());
-        query.setParameter("created", items.getCreated());
-        query.setParameter("description", items.getDescription());
-        query.setParameter("id", Integer.parseInt(id));
-        int result = query.executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+        int result;
+        try (Session session = sf.openSession()) {
+            session.beginTransaction();
+            Query query = session.createQuery("update Items set name =: name, created =: created, description =: description where id =: id");
+            query.setParameter("name", items.getName());
+            query.setParameter("created", items.getCreated());
+            query.setParameter("description", items.getDescription());
+            query.setParameter("id", Integer.parseInt(id));
+            result = query.executeUpdate();
+            session.getTransaction().commit();
+        }
         return result == 1;
     }
 
     public boolean delete(String id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Query query = session.createQuery("delete Items where id =: id");
-        query.setParameter("id", Integer.parseInt(id));
-        int result = query.executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+        int result;
+        try (Session session = sf.openSession()) {
+            session.beginTransaction();
+            Query query = session.createQuery("delete Items where id =: id");
+            query.setParameter("id", Integer.parseInt(id));
+            result = query.executeUpdate();
+            session.getTransaction().commit();
+        }
         return result == 1;
     }
 
     public List<Items> findAll() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List<Items> items = session.createQuery("from Items order by id").list();
-        session.getTransaction().commit();
-        session.close();
+        List<Items> items;
+        try (Session session = sf.openSession()) {
+            session.beginTransaction();
+            items = session.createQuery("from Items order by id").list();
+            session.getTransaction().commit();
+        }
         return items;
     }
 
     public List<Items> findByName(String key) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Query query = session.createQuery("from Items where name =: name order by id");
-        List<Items> items = query.setParameter("name", key).list();
-        session.getTransaction().commit();
-        session.close();
+        List<Items> items;
+        try (Session session = sf.openSession()) {
+            session.beginTransaction();
+            Query query = session.createQuery("from Items where name =: name order by id");
+            items = query.setParameter("name", key).list();
+            session.getTransaction().commit();
+        }
         return items;
     }
 
     public Items findById(String id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Items items = session.get(Items.class, Integer.parseInt(id));
-        session.getTransaction().commit();
-        session.close();
+        Items items;
+        try (Session session = sf.openSession()) {
+            session.beginTransaction();
+            items = session.get(Items.class, Integer.parseInt(id));
+            session.getTransaction().commit();
+        }
         return items;
     }
 
